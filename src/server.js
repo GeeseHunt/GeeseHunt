cb61c4dd74a879debd40f18ca53913b36cd1034a
Chris Xu <gzchrisxjh@gmail.com>
2020-04-27 11:52:32 -0400

    Fix mui server side rendering issue

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ import nodeFetch from 'node-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import PrettyError from 'pretty-error';
-import App from './components/App';
 import Html from './components/Html';
 import { ErrorPageWithoutStyle } from './routes/error/ErrorPage';
 import createFetch from './createFetch';
@@ -31,6 +30,7 @@ import chunks from './chunk-manifest.json'; // eslint-disable-line import/no-unr
 import configureStore from './store/configureStore';
 import { setRuntimeVariable } from './actions/runtime';
 import config from './config';
+import App from './components/App';
 
 process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at:', p, 'reason:', reason);
